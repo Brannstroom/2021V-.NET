d980c4c6b7dd2152450c6112f25b0d9df12c97a8
Peter Brännström <54285305+Brannstroom@users.noreply.github.com>
2022-01-14 10:06:43 +0100

    Add project files.

---
+++ Blogging/Blogging.DataAccess.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace Blogging.DataAccess.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public BloggingContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new();
+            connectionStringBuilder.DataSource = @"(localdb)\MSSQLLocalDB";
+            
+            
+            options.UseSqlServer();
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
---
+++ TodoApi/TodoApi/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TodoApi
+{
+    public class Car
+    {
+
+        public string name { get; set; }
+
+        public int numDoors { get; set; }
+
+        public int modelYear { get; set; }
+
+    }
+}
